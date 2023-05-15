eebb32a911927fc5a1e34f752a626fe0580de065
usmyc <hllsmtyc@gmail.com>
2023-05-15 20:52:36 +0200

    Some styling to make the page look a little nicer.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
 mod leftbar;
 mod navbar;
-use navbar::People;
+
 use yew::prelude::*;
 
 #[function_component(App)]
 fn app() -> Html {
-    let esra = People {
-        name: "Zelda".to_string(),
-        age: 20,
-    };
-
     html! {
         <div>
-        <navbar::Nav esra={esra} />
+        <navbar::Nav />
         <leftbar::Leftbar />
         </div>
     }
