f87d5c6d71a057c995802998eb6e1561cd03e5a7
usmyc <hllsmtyc@gmail.com>
2023-05-15 20:52:12 +0200

    State management added to profile icon in navbar

--- src/navbar.rs
+++ src/navbar.rs
@@ -1,21 +1,52 @@
 use yew::prelude::*;
-#[derive(Clone, PartialEq)]
-pub struct People {
-    pub name: String,
-    pub age: u8,
+struct Menu {
+    profil: String,
+    settings: String,
+    subscription: String,
+    logout: String,
+    faq: String,
 }
-
-#[derive(Properties, PartialEq)]
-pub struct NavProps {
-    pub esra: People,
-}
-
 #[function_component(Nav)]
-pub fn nav(NavProps { esra }: &NavProps) -> Html {
+pub fn nav() -> Html {
+    let menu = Menu {
+        profil: "Profil".to_string(),
+        settings: "Settings".to_string(),
+        subscription: "Subscription".to_string(),
+        logout: "Logout".to_string(),
+        faq: "FAQ".to_string(),
+    };
+    // Create a state variable with the initial value of `false`
+
+    let visible = use_state(|| false);
+    let onclick = {
+        let visible = visible.clone();
+
+        //careful about MouseEvent//
+
+        Callback::from(move |_: MouseEvent| {
+            visible.set(!*visible);
+        })
+    };
+
+    // Clone the state value to use it
+    let class = if *visible.clone() {
+        "profile-menu visible"
+    } else {
+        "profile-menu"
+    };
     html! {
        <div class="nav">
-               <p>{"Osman"}</p>
-                  <img  style="display: none;" class="usr-img" src="/assets/userimage.png" alt="userimage" />
+               <p>{"Nav"}</p>
+                  <img class="usr-img" src="/assets/userimage.png" alt="userimage" onclick={onclick} />
+                  <div class={class}>
+                    <p>{"User Name"}</p>
+                    <hr class="line"/>
+                    <p>{menu.profil}</p>
+                    <p>{menu.settings}</p>
+                    <p>{menu.subscription}</p>
+                    <p>{menu.logout}</p>
+                    <p>{menu.faq}</p>
+                  </div>
         </div>
     }
 }
